3c4bb25e84c2432bc1881aa2ed83881383e185de
unknown <abhinaykatta97@gmail.com>
2024-04-09 18:10:53 +0530

    initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,9 @@
-import { useDispatch, useSelector } from "react-redux";
-import { AppDispatch, RootState } from "./store/store";
-import {
-  decrement,
-  increment,
-  incrementAsync,
-  incrementByValue,
-} from "./store/counter/counterSlice";
-import { clearInput, setInput } from "./store/input/inputSlice";
-
-function App() {
-  const count = useSelector((state: RootState) => state.counter.value);
-  const input = useSelector((state: RootState) => state.input.value);
-  // app dispatch is needed when working with async functions
-  const dispatch = useDispatch<AppDispatch>();
+const App = () => {
   return (
-    <>
-      <div>
-        <button onClick={() => dispatch(increment())}>incre</button>
-        <button onClick={() => dispatch(incrementAsync(10))}>
-          incre async
-        </button>
-        <button onClick={() => dispatch(decrement())}>decre</button>
-        <button onClick={() => dispatch(incrementByValue(2))}>
-          incre by 2
-        </button>
-        <p>{count}</p>
-      </div>
-      <div>
-        <input
-          placeholder="enter a value"
-          value={input}
-          onChange={(e) => dispatch(setInput(e.target.value))}></input>
-        <button
-          onClick={() => {
-            dispatch(clearInput());
-          }}>
-          clear input
-        </button>
-      </div>
-
-      <p>{input ? input : "Enter an input above"}</p>
-    </>
+    <div className=" w-screen h-full min-h-screen flex flex-col justify-center items-center">
+      App
+    </div>
   );
-}
+};
 
 export default App;
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,11 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "./counter/counterSlice"; // the default export
-import inputReducer from "./input/inputSlice";
 export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-    input: inputReducer,
-  },
+  reducer: {},
 });
 
 export type RootState = ReturnType<typeof store.getState>;
