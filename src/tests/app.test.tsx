bbe65c6bd59410d7ff7fc817a9659076350b535a
unknown <abhinaykatta97@gmail.com>
2024-04-10 18:29:15 +0530

    added store beforeEach

--- src/tests/app.test.tsx
+++ src/tests/app.test.tsx
@@ -2,18 +2,31 @@ import { render, screen } from "@testing-library/react";
 import App from "../App";
 import { Provider } from "react-redux";
 import { store } from "../store/store";
+import userEvent from "@testing-library/user-event";
+const renderAppWithStore = () => {
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+};
 
-describe("", () => {
+beforeEach(() => renderAppWithStore());
+
+describe("first", () => {
   test("render app with initial state 0", () => {
-    render(
-      <Provider store={store}>
-        <App />
-      </Provider>
-    );
     const counter = screen.getByText(/counter/i);
     expect(counter).toHaveTextContent(/counter/i);
     expect(counter).toHaveTextContent(/0/i);
     expect(counter).toBeInTheDocument();
-    screen.debug();
+  });
+  test("increment on pressing increment button", async () => {
+    const incrementButton = screen.getByRole("button", { name: "incre" });
+    const decrementButton = screen.getByRole("button", { name: "decre" });
+    const counter = screen.getByText(/counter/i);
+    await userEvent.click(incrementButton);
+    expect(counter).toHaveTextContent("1");
+    await userEvent.click(decrementButton);
+    expect(counter).toHaveTextContent("0");
   });
 });
